0fc5f19a80c0dfad1393c0a2eb344ca0b86b596d
Bikramdeep Singh <bikramnehra@gmail.com>
2017-05-05 11:52:54 -0700

    changes

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var minify = require('gulp-minifier');
+ 
+gulp.task('minification', function() {
+  return gulp.src('static/**/*').pipe(minify({
+    minify: true,
+    collapseWhitespace: true,
+    conservativeCollapse: true,
+    minifyJS: true,
+    minifyCSS: true,
+    getKeptComment: function (content, filePath) {
+        var m = content.match(/\/\*![\s\S]*?\*\//img);
+        return m && m.join('\n') + '\n' || '';
+    }
+  })).pipe(gulp.dest('dest'));
+});
